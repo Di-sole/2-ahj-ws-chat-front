f7166353938514abd7643152e9d4483f6d747638
Olga Abramovich <disole@bk.ru>
2021-08-19 23:52:06 +0500

    url changed

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 import ChatWidget from './ChatWidget';
 
 const widgetContainer = document.querySelector('.chat-widget-container');
-const widget = new ChatWidget(widgetContainer, 'ws://localhost:7070');
+const widget = new ChatWidget(widgetContainer, 'wss://ahj-wschat.herokuapp.com/');
 widget.start();
